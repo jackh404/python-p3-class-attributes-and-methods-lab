cf9b32feb00271420cf6ddaf5d2a609beb358510
Jack Henza <jackhenza@gmail.com>
2023-11-21 10:26:52 -0500

    class up the methods

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,40 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+    
+    def __init__(self,name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        Song.add_to_genres(genre)
+        Song.add_to_artists(artist)
+        Song.add_to_genre_count(genre)
+        Song.add_to_artist_count(artist)
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+    @classmethod
+    def add_to_genres(cls,genre):
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+    @classmethod
+    def add_to_artists(cls,artist):
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+    @classmethod
+    def add_to_genre_count(cls,genre):
+        if genre not in Song.genre_count.keys():
+            Song.genre_count[genre] = 1
+        else:
+            Song.genre_count[genre] += 1
+    @classmethod
+    def add_to_artist_count(cls,artist):
+        if artist not in Song.artist_count.keys():
+            Song.artist_count[artist] = 1
+        else:
+            Song.artist_count[artist] += 1
